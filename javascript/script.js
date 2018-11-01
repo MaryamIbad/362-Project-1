5690da284b09a0cebbb40674534f08a40dd6e412
MaryamIbad <35270494+MaryamIbad@users.noreply.github.com>
2018-10-31 22:11:40 -0500

    updating to check for valid email

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,10 @@
 function SubmitNotify() {
-  alert('Thank You for Signing up, We look forward to seeing you!');
+   var checkEmail = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+
+  if (checkEmail.test(email.value)) {
+    alert('Thank You for Signing up, We look forward to seeing you!')
+  } else {
+    alert("Please enter a valid email address")
 }
+  //end execution
+};
