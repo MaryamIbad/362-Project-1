4dad06f185b83f5d62e991581581cfcb629786ab
MaryamIbad <35270494+MaryamIbad@users.noreply.github.com>
2018-11-01 19:12:20 -0500

    Adding Checking for valid name/email

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,24 @@
-function Submit() {
+function submit() 
+{
+   var lname = document.forms["SignUp"]["first_name_field"].value;
+   var email = document.forms["SignUp"]["email"].value;
+   
+   if (name == "")
+	{
+		
+		alert('Please enter your name');
+		return false;
+	}
+   else if (email == "")
+	{
+		alert('Please enter your Email');
+		return false;
+	}
+   else
+   {
+      
    alert("Thank you for signing up, we look forward to seeing you!");
+   }
+   
+   return true;
 }
