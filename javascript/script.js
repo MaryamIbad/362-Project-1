99d0f6dc3ac99d9214192335085505973948e234
MaryamIbad <35270494+MaryamIbad@users.noreply.github.com>
2018-11-01 19:34:05 -0500

    Fixing alert error

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,3 @@
-function submit() 
-{
-   var lname = document.forms["SignUp"]["first_name_field"].value;
-   var email = document.forms["SignUp"]["email"].value;
-   
-   if (name == "")
-	{
-		
-		alert('Please enter your name');
-		return false;
-	}
-   else if (email == "")
-	{
-		alert('Please enter your Email');
-		return false;
-	}
-   else
-   {
-      
+function submission() {
    alert("Thank you for signing up, we look forward to seeing you!");
    }
-   
-   return true;
-}
